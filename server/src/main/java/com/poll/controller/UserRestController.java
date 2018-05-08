40dd95e02419cc91d11fc8ff3e57c24bcf2ffe99
Jiaqi Qin <frank.qjq@outlook.com>
2018-05-08 12:56:30 -0700

    comment unused APIs for /user/

--- server/src/main/java/com/poll/controller/UserRestController.java
+++ server/src/main/java/com/poll/controller/UserRestController.java
@@ -46,35 +46,35 @@ public class UserRestController {
         return new ResponseEntity(responseBody, HttpStatus.OK);
     }
 
-    //-------------------Retrieve All Users--------------------------------------------------------
-
-    @RequestMapping(value = "/user/", method = RequestMethod.GET)
-    public ResponseEntity<List<AppUser>> listAllUsers() {
-        List<AppUser> appUsers = userService.findAllUsers();
-        if(appUsers.isEmpty()){
-            return new ResponseEntity<List<AppUser>>(HttpStatus.NO_CONTENT);//You many decide to return HttpStatus.NOT_FOUND
-        }
-        return new ResponseEntity<List<AppUser>>(appUsers, HttpStatus.OK);
-    }
-
-
-    //-------------------Retrieve Single AppUser--------------------------------------------------------
-
-    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<AppUser> getUser(@PathVariable("id") long id) {
-        System.out.println("Fetching user with id " + id);
-        AppUser appUser = userService.findById(id);
-        if (appUser == null) {
-            System.out.println("user with id " + id + " not found");
-            return new ResponseEntity<AppUser>(HttpStatus.NOT_FOUND);
-        }
-        return new ResponseEntity<AppUser>(appUser, HttpStatus.OK);
-    }
-
-
-
-    //-------------------Create a AppUser--------------------------------------------------------
-
+//    //-------------------Retrieve All Users--------------------------------------------------------
+//
+//    @RequestMapping(value = "/user/", method = RequestMethod.GET)
+//    public ResponseEntity<List<AppUser>> listAllUsers() {
+//        List<AppUser> appUsers = userService.findAllUsers();
+//        if(appUsers.isEmpty()){
+//            return new ResponseEntity<List<AppUser>>(HttpStatus.NO_CONTENT);//You many decide to return HttpStatus.NOT_FOUND
+//        }
+//        return new ResponseEntity<List<AppUser>>(appUsers, HttpStatus.OK);
+//    }
+//
+//
+//    //-------------------Retrieve Single AppUser--------------------------------------------------------
+//
+//    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE})
+//    public ResponseEntity<AppUser> getUser(@PathVariable("id") long id) {
+//        System.out.println("Fetching user with id " + id);
+//        AppUser appUser = userService.findById(id);
+//        if (appUser == null) {
+//            System.out.println("user with id " + id + " not found");
+//            return new ResponseEntity<AppUser>(HttpStatus.NOT_FOUND);
+//        }
+//        return new ResponseEntity<AppUser>(appUser, HttpStatus.OK);
+//    }
+//
+//
+//
+//    //-------------------Create a AppUser--------------------------------------------------------
+//
 //    @RequestMapping(value = "/user/", method = RequestMethod.POST)
 //    public @ResponseBody ResponseEntity<Void> createUser(@RequestBody AppUser appUser, UriComponentsBuilder ucBuilder) {
 //        System.out.println("Creating user " + appUser.getEmail());
@@ -92,52 +92,52 @@ public class UserRestController {
 //        headers.setLocation(ucBuilder.path("/user/{id}").buildAndExpand(appUser.getId()).toUri());
 //        return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
 //    }
-
-
-    //------------------- Update a AppUser --------------------------------------------------------
-
-    @RequestMapping(value = "/user/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<AppUser> updateUser(@PathVariable("id") long id, @RequestBody AppUser appUser) {
-        System.out.println("Updating user " + id);
-
-        AppUser currentAppUser = userService.findById(id);
-
-        if (currentAppUser ==null) {
-            System.out.println("user with id " + id + " not found");
-            return new ResponseEntity<AppUser>(HttpStatus.NOT_FOUND);
-        }
-
-        currentAppUser.setRole(appUser.getRole());
-
-        userService.updateUser(currentAppUser);
-        return new ResponseEntity<AppUser>(currentAppUser, HttpStatus.OK);
-    }
-
-    //------------------- Delete a AppUser --------------------------------------------------------
-
-    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<AppUser> deleteUser(@PathVariable("id") long id) {
-        System.out.println("Fetching & Deleting user with id " + id);
-
-        AppUser appUser = userService.findById(id);
-        if (appUser == null) {
-            System.out.println("Unable to delete. user with id " + id + " not found");
-            return new ResponseEntity<AppUser>(HttpStatus.NOT_FOUND);
-        }
-
-        userService.deleteUserById(id);
-        return new ResponseEntity<AppUser>(HttpStatus.NO_CONTENT);
-    }
-
-
-    //------------------- Delete All Users --------------------------------------------------------
-
-    @RequestMapping(value = "/user/", method = RequestMethod.DELETE)
-    public ResponseEntity<AppUser> deleteAllUsers() {
-        System.out.println("Deleting All Users");
-
-        userService.deleteAllUsers();
-        return new ResponseEntity<AppUser>(HttpStatus.NO_CONTENT);
-    }
+//
+//
+//    //------------------- Update a AppUser --------------------------------------------------------
+//
+//    @RequestMapping(value = "/user/{id}", method = RequestMethod.PUT)
+//    public ResponseEntity<AppUser> updateUser(@PathVariable("id") long id, @RequestBody AppUser appUser) {
+//        System.out.println("Updating user " + id);
+//
+//        AppUser currentAppUser = userService.findById(id);
+//
+//        if (currentAppUser ==null) {
+//            System.out.println("user with id " + id + " not found");
+//            return new ResponseEntity<AppUser>(HttpStatus.NOT_FOUND);
+//        }
+//
+//        currentAppUser.setRole(appUser.getRole());
+//
+//        userService.updateUser(currentAppUser);
+//        return new ResponseEntity<AppUser>(currentAppUser, HttpStatus.OK);
+//    }
+//
+//    //------------------- Delete a AppUser --------------------------------------------------------
+//
+//    @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
+//    public ResponseEntity<AppUser> deleteUser(@PathVariable("id") long id) {
+//        System.out.println("Fetching & Deleting user with id " + id);
+//
+//        AppUser appUser = userService.findById(id);
+//        if (appUser == null) {
+//            System.out.println("Unable to delete. user with id " + id + " not found");
+//            return new ResponseEntity<AppUser>(HttpStatus.NOT_FOUND);
+//        }
+//
+//        userService.deleteUserById(id);
+//        return new ResponseEntity<AppUser>(HttpStatus.NO_CONTENT);
+//    }
+//
+//
+//    //------------------- Delete All Users --------------------------------------------------------
+//
+//    @RequestMapping(value = "/user/", method = RequestMethod.DELETE)
+//    public ResponseEntity<AppUser> deleteAllUsers() {
+//        System.out.println("Deleting All Users");
+//
+//        userService.deleteAllUsers();
+//        return new ResponseEntity<AppUser>(HttpStatus.NO_CONTENT);
+//    }
 
 }
